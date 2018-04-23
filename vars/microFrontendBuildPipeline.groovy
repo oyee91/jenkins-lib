2a3b68b234ee4c29f41f859570ec77da1fa65b69
Gábor Takács <gabor.takacs@semcon.com>
2018-04-23 15:01:36 +0200

    Lift variable scope

--- vars/microFrontendBuildPipeline.groovy
+++ vars/microFrontendBuildPipeline.groovy
@@ -8,20 +8,24 @@ def call(body) {
     body.delegate = config
     body()
 
+    def version
+    def name
+
     dockerNode(dockerImage: 'stakater/frontend-tools:latest') {
         container(name: 'docker') {
             stage("Checkout") {
                 checkout scm
                 def js_package = readJSON file: 'package.json'
                 def version_old = js_package.version.tokenize(".")
-                def version_new = "${version_old[0]}.${version_old[1]}.${env.BUILD_NUMBER}"
-                currentBuild.displayName = "${version_new}"
+                version = "${version_old[0]}.${version_old[1]}.${env.BUILD_NUMBER}"
+                name = js_package.name
+                currentBuild.displayName = "${name}/${version}"
             }
 
             stage("Install") {
                 withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
                                   variable: 'NEXUS_NPM_AUTH']]) {
-                    sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn version --no-git-tag-version --new-version ${version_new}"
+                    sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn version --no-git-tag-version --new-version ${version}"
                     sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn install"
                 }
             }
@@ -67,7 +71,7 @@ def call(body) {
 //                }
 
             stage("Deploy") {
-                s3Upload(file: 'lib/', bucket: '847616476486-microfrontends2', path: "${js_package.name}/${version_new}/")
+                s3Upload(file: 'lib/', bucket: '847616476486-microfrontends2', path: "${name}/${version}/")
             }
 
         }
