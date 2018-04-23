69f06ba108f5772a1ef8374ebbbaa3fc51a13d22
Gábor Takács <gabor.takacs@semcon.com>
2018-04-23 14:52:20 +0200

    Add back most of the pipeline

--- vars/microFrontendBuildPipeline.groovy
+++ vars/microFrontendBuildPipeline.groovy
@@ -18,53 +18,53 @@ def call(body) {
                     def version_new = "${version_old[0]}.${version_old[1]}.${env.BUILD_NUMBER}"
                     currentBuild.displayName = "${version_new}"
                 }
-//
-//                stage("Install") {
+
+                stage("Install") {
+                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                      variable: 'NEXUS_NPM_AUTH']]) {
+                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn version --no-git-tag-version --new-version ${version_new}"
+                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn install"
+                    }
+                }
+
+                stage("Lint") {
+                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                      variable: 'NEXUS_NPM_AUTH']]) {
+                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn lint -f junit -o lint-report.xml"
+                    }
+                }
+
+                stage("Test") {
+                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                      variable: 'NEXUS_NPM_AUTH']]) {
+                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn test-ci"
+                    }
+                }
+
+                stage("Tag") {
+
+                }
+
+                stage("Build") {
+                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                      variable: 'NEXUS_NPM_AUTH']]) {
+                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn build"
+                    }
+                }
+
+//                stage("Publish to nexus") {
 //                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
 //                                      variable: 'NEXUS_NPM_AUTH']]) {
-//                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn version --no-git-tag-version --new-version ${version_prefix}"
-//                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn install"
+//                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} npm publish"
 //                    }
 //                }
-//
-//                stage("Lint") {
-//                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-//                                      variable: 'NEXUS_NPM_AUTH']]) {
-//                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn lint -f junit -o lint-report.xml"
-//                    }
-//                }
-//
-//                stage("Test") {
-//                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-//                                      variable: 'NEXUS_NPM_AUTH']]) {
-//                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn test-ci"
-//                    }
-//                }
-//
-//                stage("Tag") {
-//
-//                }
-//
-//                stage("Build") {
-//                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-//                                      variable: 'NEXUS_NPM_AUTH']]) {
-//                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn build"
-//                    }
-//                }
-//
-////                stage("Publish to nexus") {
-////                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-////                                      variable: 'NEXUS_NPM_AUTH']]) {
-////                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} npm publish"
-////                    }
-////                }
-//
-//                stage("Deploy") {
-//                    s3Upload(file: 'lib/', bucket: '847616476486-microfrontends2', path: "${js_package}/${version_prefix}/")
-//                }
+
+                stage("Deploy") {
+                    s3Upload(file: 'lib/', bucket: '847616476486-microfrontends2', path: "${js_package.name}/${version_new}/")
+                }
             } finally {
-//                junit 'lint-report.xml'
-//                junit 'junit.xml'
+                junit 'lint-report.xml'
+                junit 'junit.xml'
             }
         }
     }
